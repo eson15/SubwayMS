cc091ab8b56c1ed5bf357e6b5df447c42a8c2a4d
zx <489588268@qq.com>
2017-01-13 14:15:42 +0800

    fix returnCode:singlecardcannotcharge in subwayFillChargeProc.cpp

--- subwayCharge/subwayCommand/subwayCmdProc/src/subwayFillChargeProc.cpp
+++ subwayCharge/subwayCommand/subwayCmdProc/src/subwayFillChargeProc.cpp
@@ -19,7 +19,7 @@ using namespace std;
 */
 void ProcFillChargeCmd(UN_CMD &unCmd, char returnStr[MAX_SEND_BUFFER_LENGTH])
 {
-    
+
 	unsigned int balance = 0;
 	EN_CARD_TYPE enCard = EN_CARD_TYPE_NOMAL;
 
@@ -32,21 +32,21 @@ void ProcFillChargeCmd(UN_CMD &unCmd, char returnStr[MAX_SEND_BUFFER_LENGTH])
 		return;
 	}
 	//卡类型判断
-	if(enCard == EN_CARD_TYPE_SINGLE)
+	if (EN_CARD_TYPE_SINGLE == enCard)
 	{
-		returnCode = EN_RETURN_INPUT_INVALID_CARDTYPE;
+		returnCode = EN_RETURN_SINGLE_CARD_CANNOT_RECHARGE;
 		GetOutputResultStr(EN_CMD_TYPE_FILL_CHARGE, returnCode, unCmd.stCmdFillCharge.cardNo, enCard, balance, returnStr);
 		return;
 	}
 
-    //进行充值，包括充值范围，以及最后的余额范围判断，充值成功则更新卡余额
+	//进行充值，包括充值范围，以及最后的余额范围判断，充值成功则更新卡余额
 	returnCode = RechargeCard(unCmd.stCmdFillCharge.cardNo, unCmd.stCmdFillCharge.cardCharge);
 
-    //根据充值后的卡信息  GetCardInfo
+	//根据充值后的卡信息  GetCardInfo
 	GetCardInfo(unCmd.stCmdFillCharge.cardNo, balance, enCard);
-    //输出字符串
-    //GetOutputResultStr(EN_CMD_TYPE_FILL_CHARGE, returnCode, pCmdCharge->cardNo, enCard, balance, returnStr);
+	//输出字符串
+	//GetOutputResultStr(EN_CMD_TYPE_FILL_CHARGE, returnCode, pCmdCharge->cardNo, enCard, balance, returnStr);
 	GetOutputResultStr(EN_CMD_TYPE_FILL_CHARGE, returnCode, unCmd.stCmdFillCharge.cardNo, enCard, balance, returnStr);
 
-    return;
+	return;
 }
